9f44f6a55ae10f27c0f1f506d5c7bc3f4e7c7b3a
Matthew Kaplan <kaplan.matthew.p@gmail.com>
2019-08-20 01:25:40 -0600

    Added cors

--- src/app.js
+++ src/app.js
@@ -1,8 +1,11 @@
 const express = require('express');
 require('./db/mongoose');
 const app = express();
+const cors = require('cors')
 const notesRouter = require('./routes/notes');
 
+
+app.use(cors());
 app.use(express.json());
 app.use(notesRouter);
 
