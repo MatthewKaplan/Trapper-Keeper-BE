10268a604942ad34159e4729a761b659c369e259
Matthew Kaplan <kaplan.matthew.p@gmail.com>
2019-08-19 23:48:46 -0600

    Move start up into index.js and out of app

--- src/app.js
+++ src/app.js
@@ -1,12 +1,9 @@
 const express = require('express');
-const app = express();
 require('./db/mongoose');
+const app = express();
 const notesRouter = require('./routes/notes');
 
-const port = process.env.PORT || 3000;
 app.use(express.json());
 app.use(notesRouter);
 
-app.listen(port, () => {
-	console.log(`Listening on port ${port}`);
-});
+module.exports = app;
